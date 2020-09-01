7f887a74648ff3595f5d1acea4ef39fd63c62de6
akash shahriar <akashshahriar55@gmail.com>
2020-09-02 00:01:15 +0600

    starting of the project

---
+++ app/src/main/java/com/cookietech/chordera/MainActivity.java
@@ -0,0 +1,39 @@
+package com.cookietech.chordera;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.cookietech.chordera.Splash.SplashFragment;
+import com.cookietech.chordera.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    SplashFragment splashFragment;
+    FragmentManager fragmentManager;
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View root = binding.getRoot();
+        setContentView(root);
+
+        splashFragment = SplashFragment.newInstance();
+        fragmentManager = getSupportFragmentManager();
+        addFragmentToBackStack(splashFragment,splashFragment.getTag(),binding.mainFragmentHolder.getId());
+    }
+
+    public void addFragmentToBackStack(Fragment fragment, String tag, int containerViewId) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(binding.mainFragmentHolder.getId(), fragment);
+        fragmentTransaction.commit();
+    }
+}
---
+++ app/src/main/java/com/cookietech/chordera/Splash/SplashFragment.java
@@ -0,0 +1,92 @@
+package com.cookietech.chordera.Splash;
+
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.alphamovie.lib.AlphaMovieView;
+import com.cookietech.chordera.R;
+import com.cookietech.chordera.databinding.FragmentSplashBinding;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SplashFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SplashFragment extends Fragment {
+
+    FragmentSplashBinding binding;
+
+    public static SplashFragment newInstance() {
+        SplashFragment fragment = new SplashFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentSplashBinding.inflate(getLayoutInflater(),container,false);
+        View root = binding.getRoot();
+
+
+        return root;
+    }
+
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        Uri video = Uri.parse("android.resource://" + getContext().getPackageName() + "/"
+                + R.raw.chordera_animation);
+
+        binding.videoView.setLooping(true);
+        binding.videoView.setVideoFromUri(getContext(),video);
+        binding.videoView.setOnVideoStartedListener(new AlphaMovieView.OnVideoStartedListener() {
+            @Override
+            public void onVideoStarted() {
+                Log.d("akash_debug", "onVideoStarted: ");
+                new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        binding.hideScreen.setVisibility(View.INVISIBLE);
+                    }
+                },1000);
+
+            }
+        });
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        binding.videoView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        binding.videoView.onPause();
+    }
+}
