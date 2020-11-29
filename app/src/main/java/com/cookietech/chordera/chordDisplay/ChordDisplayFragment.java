e21209928479eb0d92d9f4b5c0b0f38c85df0af4
DESKTOP-4UOPMEB\bisha <bishalimtiaz60@gmail.com>
2020-11-30 01:35:24 +0600

    ight and dark mode done

--- app/src/main/java/com/cookietech/chordera/chordDisplay/ChordDisplayFragment.java
+++ app/src/main/java/com/cookietech/chordera/chordDisplay/ChordDisplayFragment.java
@@ -2,24 +2,37 @@ package com.cookietech.chordera.chordDisplay;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 
 import com.cookietech.chordera.R;
 import com.cookietech.chordera.databinding.FragmentChordDisplayBinding;
 import com.cookietech.chordera.fragments.ChorderaFragment;
+import com.cookietech.chordlibrary.Chord;
+import com.cookietech.chordlibrary.ChordsAdapter;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ChordDisplayFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ChordDisplayFragment extends ChorderaFragment {
+public class ChordDisplayFragment extends ChorderaFragment implements ChordsAdapter.Communicator {
 
     FragmentChordDisplayBinding binding;
+    private  ChordsAdapter chordsAdapter;
+    ArrayList<Chord> chords =new ArrayList<>();
+    private boolean isDarkModeActivated = false;
 
 
     public ChordDisplayFragment() {
@@ -48,5 +61,207 @@ public class ChordDisplayFragment extends ChorderaFragment {
         // Inflate the layout for this fragment
         binding = FragmentChordDisplayBinding.inflate(inflater,container,false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        setDummyChords();
+        binding.rvChords.setHasFixedSize(true);
+        GridLayoutManager layoutManager = new GridLayoutManager(requireContext(),5);
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
+        binding.rvChords.setLayoutManager(layoutManager);
+        chordsAdapter = new ChordsAdapter(requireContext(),chords,this,binding.rvChords);
+        binding.rvChords.setAdapter(chordsAdapter);
+
+        toggleMode();
+        binding.modeSwitch.setChecked(isDarkModeActivated);
+        binding.modeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                isDarkModeActivated = isChecked;
+                toggleMode();
+
+            }
+        });
+
+    }
+
+    private void setDummyChords(){
+        chords.clear();
+        chords.add(new Chord("A maj", new ArrayList<Integer>(){
+            {
+                add(-1);
+                add(0);
+                add(2);
+                add(2);
+                add(2);
+                add(0);
+            }
+        },new ArrayList<Integer>(){
+            {
+                add(0);
+                add(0);
+                add(2);
+                add(3);
+                add(4);
+                add(0);
+            }
+        }));
+
+        chords.add(new Chord("A min", new ArrayList<Integer>(){
+            {
+                add(-1);
+                add(0);
+                add(2);
+                add(2);
+                add(1);
+                add(0);
+            }
+        },new ArrayList<Integer>(){
+            {
+                add(0);
+                add(0);
+                add(2);
+                add(3);
+                add(1);
+                add(0);
+            }
+        }));
+
+        chords.add(new Chord("A5", new ArrayList<Integer>(){
+            {
+                add(-1);
+                add(0);
+                add(2);
+                add(2);
+                add(-1);
+                add(-1);
+            }
+        },new ArrayList<Integer>(){
+            {
+                add(0);
+                add(0);
+                add(2);
+                add(3);
+                add(0);
+                add(0);
+            }
+        }));
+
+        chords.add(new Chord("A maj", new ArrayList<Integer>(){
+            {
+                add(-1);
+                add(0);
+                add(2);
+                add(2);
+                add(2);
+                add(0);
+            }
+        },new ArrayList<Integer>(){
+            {
+                add(0);
+                add(0);
+                add(2);
+                add(3);
+                add(4);
+                add(0);
+            }
+        }));
+
+        chords.add(new Chord("A min", new ArrayList<Integer>(){
+            {
+                add(-1);
+                add(0);
+                add(2);
+                add(2);
+                add(1);
+                add(0);
+            }
+        },new ArrayList<Integer>(){
+            {
+                add(0);
+                add(0);
+                add(2);
+                add(3);
+                add(1);
+                add(0);
+            }
+        }));
+
+        chords.add(new Chord("A5", new ArrayList<Integer>(){
+            {
+                add(-1);
+                add(0);
+                add(2);
+                add(2);
+                add(-1);
+                add(-1);
+            }
+        },new ArrayList<Integer>(){
+            {
+                add(0);
+                add(0);
+                add(2);
+                add(3);
+                add(0);
+                add(0);
+            }
+        }));
+
+        chords.add(new Chord("A min", new ArrayList<Integer>(){
+            {
+                add(-1);
+                add(0);
+                add(2);
+                add(2);
+                add(1);
+                add(0);
+            }
+        },new ArrayList<Integer>(){
+            {
+                add(0);
+                add(0);
+                add(2);
+                add(3);
+                add(1);
+                add(0);
+            }
+        }));
+    }
+
+    @Override
+    public void onChordSelected(int position) {
+
+    }
+
+    private void activateLightMode(){
+        binding.rootContainer.setBackgroundColor(getResources().getColor(R.color.white));
+        binding.tvSongName.setTextColor(getResources().getColor(R.color.colorPrimary));
+        binding.tvTuning.setTextColor(getResources().getColor(R.color.colorPrimary));
+        binding.tvKey.setTextColor(getResources().getColor(R.color.colorPrimary));
+        binding.tvChords.setTextColor(getResources().getColor(R.color.colorPrimary));
+        binding.tvSongChords.setTextColor(getResources().getColor(R.color.colorPrimary));
+    }
+
+    private void activateDarkMode(){
+        binding.rootContainer.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        binding.tvSongName.setTextColor(getResources().getColor(R.color.white));
+        binding.tvTuning.setTextColor(getResources().getColor(R.color.white));
+        binding.tvKey.setTextColor(getResources().getColor(R.color.white));
+        binding.tvChords.setTextColor(getResources().getColor(R.color.white));
+        binding.tvSongChords.setTextColor(getResources().getColor(R.color.white));
+    }
+
+    private void toggleMode() {
+
+        if (!isDarkModeActivated){
+            activateLightMode();
+        }
+        else {
+            activateDarkMode();
+        }
     }
 }
